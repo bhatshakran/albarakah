3bbb50c2945e363bd07db763244fc0b5c7818052
Shaqran Bhat <bhatshaqran@protonmail.com>
2026-04-27 10:59:08 +0530

    feat: :sparkles: add nitro.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tailwindcss()],
+  plugins: [tanstackStart(), react(), tailwindcss(), nitro()],
 
   resolve: {
     alias: {
